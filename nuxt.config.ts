64758ba62a27484df3e1dc616e5048b774e395cf
uDaiko <uDegr@proton.me>
2024-09-19 03:56:59 +0200

    installed supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/ui', '@nuxt/eslint']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxtjs/supabase'
+  ]
 })
